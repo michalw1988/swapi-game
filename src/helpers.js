baeb6f50e42ee727df1b18578fed1058bbccf819
Michal Wisniewski <michal.wisniewski@softnauts.com>
2019-11-09 19:47:24 +0100

    added unit tests for helper methods

--- src/helpers.js
+++ src/helpers.js
@@ -1,4 +1,4 @@
-const getRandomInt = (min, max) => {
+export const getRandomInt = (min, max) => {
   min = Math.ceil(min)
   max = Math.floor(max)
   return Math.floor(Math.random() * (max - min + 1)) + min
---
+++ src/helpers.test.js
@@ -0,0 +1,38 @@
+import { getRandomPairNumbers, getRandomInt } from './helpers'
+
+describe('Getting pair of number', () => {
+  it('returns pair of number', () => {
+    const pairOfNumbers = getRandomPairNumbers(10);
+    expect(pairOfNumbers.length).toEqual(2)
+  });
+
+  it('returns numbers leower or equal to maximum', () => {
+    const max = 3
+    const pairOfNumbers = getRandomPairNumbers(max)
+    expect(pairOfNumbers[0]).toBeLessThanOrEqual(max)
+    expect(pairOfNumbers[1]).toBeLessThanOrEqual(max)
+  });
+
+  it('returns different numbers', () => {
+    const pairOfNumbers = getRandomPairNumbers(2)
+    expect(pairOfNumbers[0]).not.toEqual(pairOfNumbers[1])
+  });
+})
+
+describe('Getting ineger value from given range', () => {
+  it('returns number in given range', () => {
+    const randomNumber = getRandomInt(1, 3)
+    expect(randomNumber).toBeGreaterThanOrEqual(1)
+    expect(randomNumber).toBeLessThanOrEqual(3)
+  });
+
+  it('returns number in given range', () => {
+    const randomNumber = getRandomInt(10, 10)
+    expect(randomNumber).toEqual(10)
+  });
+
+  it('returns integer value', () => {
+    const randomNumber = getRandomInt(0, 10)
+    expect(Number.isInteger(randomNumber)).toEqual(true)
+  });
+})
